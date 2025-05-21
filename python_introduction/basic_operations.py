b71f72d5232d82b7f0b85e03f4bc9ec9d1409a28
Anthony <anthonyreagan126@gmail.com>
2025-05-21 16:50:21 +0300

    adding operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+
+number_1 = 10
+number_2 = 5
+
+addition = number_1 + number_2
+subtraction = number_1 - number_2
+multiplication = number_1 * number_2
+
+
+print (f"Addition of {number_1} and {number_2} {addition} ")
+print (f"Subtraction of {number_1} and {number_2} {subtraction} ")
+print (f"Multplication of {number_1} and {number_2} {multiplication} ")
+
