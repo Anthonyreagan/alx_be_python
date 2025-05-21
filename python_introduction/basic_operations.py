9abfa3c5a34fd0e6c7d14094874bde0a112907cb
Anthony Simiyu <127312927+Anthonyreagan@users.noreply.github.com>
2025-05-21 16:53:59 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,13 @@
 
-number_1 = 10
-number_2 = 5
+number1 = 10
+number2 = 5
 
-addition = number_1 + number_2
-subtraction = number_1 - number_2
-multiplication = number_1 * number_2
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
 
 
-print (f"Addition of {number_1} and {number_2} {addition} ")
-print (f"Subtraction of {number_1} and {number_2} {subtraction} ")
-print (f"Multplication of {number_1} and {number_2} {multiplication} ")
+print (f"Addition of {number1} and {number2} {addition} ")
+print (f"Subtraction of {number1} and {number2} {subtraction} ")
+print (f"Multplication of {number1} and {number2} {multiplication} ")
 
