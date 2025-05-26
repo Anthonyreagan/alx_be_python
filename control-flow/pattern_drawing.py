c4a3b1a2b84271ba7c6d9f5738124eb8724b4145
Anthony <anthonyreagan126@gmail.com>
2025-05-27 01:05:11 +0300

    input a positive integer that represents the size of the pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,12 +1,10 @@
+
 size = int(input("Enter the size of the pattern: "))
+
 row = 0
-
-
 while row < size:
-    for i in range(size):
-        asterics = i * '*'
-        print(f"{asterics}", end="")
+    for col in range(size):
+        print("*", end="")
+
     print()
     row += 1
-
-
