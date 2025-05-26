2aaf1d62c3d0933b3b012a49f3cea92178624ba4
Anthony <anthonyreagan126@gmail.com>
2025-05-27 00:01:56 +0300

    use Match Case statements for handling multiple operations in a simple calculator program.

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,28 +1,25 @@
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+def main():
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Choose the operation (+, -, *, /): ")
 
-operation = input("Choose the operation (+, -, *, /):. ")
+        match operation:
+            case "+":
+                print(f"The result is {num1 + num2}.")
+            case "-":
+                print(f"The result is {num1 - num2}.")
+            case "*":
+                print(f"The result is {num1 * num2}.")
+            case "/":
+                if num2 == 0:
+                    print("Cannot divide by zero.")
+                else:
+                    print(f"The result is {num1 / num2}.")
+            case _:
+                print("Invalid operation. Please choose from +, -, *, /.")
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
 
-match operation:
-    case "+":
-        result = num1 + num2
-        print(f"The result is: {result}")
-    case "-":
-        result = num1 - num2
-        print(f"The result is: {result}")
-    case "*":
-        result = num1 * num2
-        print(f"The result is: {result}")
-    case "/":
-        if  num2 == 0:
-            print("Cannot divide by zero. ")
-        else:
-            result = num1 / num2
-            print(f"The result is: {result:.2f}")
-    case _:
-        print("Invalid operation. Please choose from +, -, *, /.")
-
-
-
-
-
+if __name__ == "__main__":
+    main()
