8ed83b79227fcb0a74b32c1c27a2df1290230852
Anthony <anthonyreagan126@gmail.com>
2025-06-04 16:49:47 +0300

    Python’s datetime module by writing a script that performs specified operations with dates and times, and calculating future dates with timedelta

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,21 @@
+import datetime
+from datetime import timedelta
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    formated_date_time = current_date.strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"current date and time is: {formated_date_time}")
+
+def calculate_future_date():
+    current_date = datetime.datetime.now()
+    number_of_days = int(input("Enter a number of days: ").strip().lower())
+    future_date = current_date + timedelta(number_of_days)
+    formated_future_date = future_date.strftime("%Y-%m-%d")
+
+    print(f"date after {number_of_days} days will be: {formated_future_date}")
+    
+
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
